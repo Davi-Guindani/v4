c9c1647aa1483e8a80ff1571626115bdfbd81eef
Davi Guindani <davigpvieira@gmail.com>
2024-06-11 13:55:16 -0300

    inicial build

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, render_template
+import pandas as pd
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    class_name = request.form.get('class')
+    date = request.form.get('date')
+    teacher = request.form.get('teacher')
+    list_items = request.form.get('list')
+
+    # Verifica se o arquivo já existe
+    file_exists = os.path.isfile('dados.xlsx')
+
+    # Se o arquivo não existir, cria um novo DataFrame e salva
+    if not file_exists:
+        df = pd.DataFrame(columns=['Turma', 'Data', 'Professor', 'Lista'])
+        df.to_excel('dados.xlsx', index=False)
+
+    # Carrega os dados existentes
+    df = pd.read_excel('dados.xlsx')
+
+    # Cria um novo DataFrame com os novos dados
+    new_data = pd.DataFrame({'Turma': [class_name], 'Data': [date], 'Professor': [teacher], 'Lista': [list_items]})
+
+    # Concatena os DataFrames
+    df = pd.concat([df, new_data], ignore_index=True)
+
+    # Salva os dados atualizados no arquivo
+    df.to_excel('dados.xlsx', index=False)
+
+    print(f'Turma: {class_name}')
+    print(f'Data: {date}')
+    print(f'Professor: {teacher}')
+    print(f'Lista: {list_items}')
+    
+    return f'Dados recebidos e armazenados: Turma - {class_name}, Data - {date}, Professor - {teacher}, Lista - {list_items}'
+
+if __name__ == '__main__':
+    app.run(debug=True)
