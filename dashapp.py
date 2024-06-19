27a48db6dc71c49530e72b2c256884a643b632b3
Davi Guindani <davigpvieira@gmail.com>
2024-06-19 10:42:47 -0300

    fazendo o dash funcionar (presenças/turma/dia)

--- dashapp.py
+++ dashapp.py
@@ -1,36 +1,61 @@
 from dash import Dash, html, dcc, Input, Output
-import plotly.express as px
+import plotly.graph_objs as go
 import pandas as pd
+from collections import Counter
+from datetime import datetime
 
-def create_dash_app(flask_app):
-    dash_app = Dash(server=flask_app, url_base_pathname='/dash/')
+def create_dash_app(server):
+    dash_app = Dash(server=server, url_base_pathname='/dash/')
+
+    attendances_response = server.supabase.table('ATTENDANCES').select('*').eq('class_id', 8).order('date').execute()
+    attendances_response = attendances_response.data
+    datas = list([at['date'] for at in attendances_response])
+    datas = [datetime.strptime(data, '%Y-%m-%d').date() for data in datas]
+
+    atids = list([at['id'] for at in attendances_response])
+    attendances_students_response = server.supabase.table('ATTENDANCES_STUDENTS').select('*').in_('attendance_id', atids).execute()
+    attendances_students_response = attendances_students_response.data
+    attendances_students_response = sorted(attendances_students_response, key=lambda x: x['attendance_id'])
+    attendace_students_counts = Counter(item['attendance_id'] for item in attendances_students_response)
+    occurrence_list = list(attendace_students_counts.values())
     
-    # Obter dados de estudantes do Supabase
-    students_response = flask_app.supabase.table('STUDENTS').select('*').execute()
-    students = students_response.data
+    df = pd.DataFrame(
+        {
+            'datas': datas,
+            'qtd': occurrence_list
+        }
+    )
 
-    # Criar DataFrame Pizza H-M
-    students_df = pd.DataFrame(students)
+    figteste = go.Figure(go.Bar(x=df['datas'], y=df['qtd']))
+    figteste.layout = dict(xaxis=dict(type="category"))
 
-    # Gráfico de pizza para porcentagem de homens e mulheres
-    genre_counts = students_df['genre'].value_counts(normalize=True) * 100
-    genre_counts = genre_counts.reset_index()
-    genre_counts.columns = ['genre', 'percentage']
-    fig_pie = px.pie(genre_counts, values='percentage', names='genre', title='Porcentagem de Homens e Mulheres')
+    attendances_response = server.supabase.table('ATTENDANCES').select('*').eq('class_id', 1).order('date').execute()
+    attendances_response = attendances_response.data
+    datas = list([at['date'] for at in attendances_response])
+    datas = [datetime.strptime(data, '%Y-%m-%d').date() for data in datas]
 
-    data = {
-        'frutas': ['Maçã', 'Banana', 'Laranja', 'Uva', 'Pêssego'],
-        'qtd': [10, 5, 8, 12, 3]
-    }
-    # Criar DataFrame Frutas
-    df = pd.DataFrame(data)
+    atids = list([at['id'] for at in attendances_response])
+    attendances_students_response = server.supabase.table('ATTENDANCES_STUDENTS').select('*').in_('attendance_id', atids).execute()
+    attendances_students_response = attendances_students_response.data
+    attendances_students_response = sorted(attendances_students_response, key=lambda x: x['attendance_id'])
+    attendace_students_counts = Counter(item['attendance_id'] for item in attendances_students_response)
+    occurrence_list = list(attendace_students_counts.values())
+    
+    df = pd.DataFrame(
+        {
+            'datas': datas,
+            'qtd': occurrence_list
+        }
+    )
 
-    fig = px.bar(df, x='frutas', y='qtd')
+    figteste2 = go.Figure(go.Bar(x=df['datas'], y=df['qtd']))
+    figteste2.layout = dict(xaxis=dict(type="category"))
 
     dash_app.layout = html.Div([
-        html.H1('Dashboardzada'),
-        dcc.Graph(id='grafico', figure=fig),
-        dcc.Graph(id='grafico_pie', figure=fig_pie)
+        html.H1('Turma 20:00 às 21:15'),
+        dcc.Graph(id='grafico teste', figure=figteste),
+        html.H1('Turma 18:45 às 20:00'),
+        dcc.Graph(id='grafico teste 2', figure=figteste2)
     ])
     
     return dash_app
