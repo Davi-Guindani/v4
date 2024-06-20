28a71645c8c4d50877b2f93300712217136fb4a0
Davi Guindani <davigpvieira@gmail.com>
2024-06-20 19:10:39 -0300

    criando interface supabase e testando remoção de pacotes

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from dotenv import load_dotenv
 import os
+from supabase import create_client
 
 app = Flask(__name__)
+
 load_dotenv()
+app.supabase = create_client(os.getenv('SUPABASE_URL'), os.getenv('SUPABASE_KEY'))
+
+
 
 @app.route('/')
 def hello_world():
