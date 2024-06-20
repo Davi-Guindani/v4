a7d85b570f33304809808a89d6ebe30e3c6ba564
Davi Guindani <davigpvieira@gmail.com>
2024-06-20 19:04:08 -0300

    testando acesso a variavel de ambiente em deploy

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 load_dotenv()
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return os.getenv('SUPABASE_URL')
