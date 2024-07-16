4097742abba05bba4302df1e02d3dca15e758daa
Davi Guindani <davigpvieira@gmail.com>
2024-07-16 19:10:20 -0300

    funcionalidade de cadastro e login

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-from supabase import create_client
+from flask import Flask, render_template, request, redirect, url_for
+import supabase
+
 from dotenv import load_dotenv
 import os
 
@@ -7,19 +8,47 @@ app = Flask(__name__)
 
 # Creating Supabase client with env vars
 load_dotenv()
-app.supabase = create_client(os.getenv('SUPABASE_URL'), os.getenv('SUPABASE_KEY'))
+app.supabase = supabase.create_client(os.getenv('SUPABASE_URL'), os.getenv('SUPABASE_KEY'))
 
-@app.route('/')
+@app.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user_data = {'email': email, 'password': password}
 
-    return render_template("sign_up.html")
+        try:
+            response = app.supabase.auth.sign_up(user_data)
+            return redirect(url_for('success'))
+        except Exception as e:
+            return str(e)
 
-@app.route('/login')
+    return render_template('sign_up.html')
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user_data = {'email': email, 'password': password}
 
-    return render_template("login.html")
+        try:
+            response = app.supabase.auth.sign_in_with_password(user_data)
+            return redirect(url_for('welcome'))
+        except Exception as e:
+            return str(e)
+    
+    return render_template('login.html')
+
+@app.route('/welcome')
+def welcome():
+    return "Bem-vindo! Login realizado com sucesso."
 
 if __name__ == "__main__":
 
-    app.run(debug=False)
+    app.run(debug=True)
     # app.run(debug=False,host='0.0.0.0', port=80)
