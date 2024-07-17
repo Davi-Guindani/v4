49d2041d554fac423e0674511a73e65068c31ca6
Davi Guindani <davigpvieira@gmail.com>
2024-07-17 15:09:31 -0300

    teste de autenticação

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import supabase
-
 from dotenv import load_dotenv
 import os
 
 app = Flask(__name__)
 
-# Creating Supabase client with env vars
 load_dotenv()
+
+app.secret_key = os.getenv('SECRET_KEY')
+
 app.supabase = supabase.create_client(os.getenv('SUPABASE_URL'), os.getenv('SUPABASE_KEY'))
 
 @app.route('/sign_up', methods=['GET', 'POST'])
@@ -37,7 +38,8 @@ def login():
         user_data = {'email': email, 'password': password}
 
         try:
-            session = app.supabase.auth.sign_in_with_password(user_data)
+            response = app.supabase.auth.sign_in_with_password(user_data)
+            session['user_id'] = response.user.id 
             return redirect(url_for('welcome'))
         except Exception as e:
             return str(e)
@@ -46,9 +48,22 @@ def login():
 
 @app.route('/welcome')
 def welcome():
-    return "Bem-vindo! Login realizado com sucesso."
+    if 'user_id' in session:
+        return "Bem-vindo! Login realizado com sucesso."
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/protected')
+def protected():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    
+    return "Esta é uma página protegida. Você está logado."
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
-
     app.run(debug=True)
-    # app.run(debug=False,host='0.0.0.0', port=80)
