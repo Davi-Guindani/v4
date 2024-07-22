de3185fcdc7763461bc4506f59bc036b3bebafad
Davi Guindani <davigpvieira@gmail.com>
2024-07-22 13:59:56 -0300

    tipagem de usuários

--- app.py
+++ app.py
@@ -4,23 +4,27 @@ from dotenv import load_dotenv
 import os
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # Defina sua secret key
 
+# Carregar variáveis de ambiente
 load_dotenv()
 
+# Criar cliente Supabase com variáveis de ambiente
 app.supabase = supabase.create_client(os.getenv('SUPABASE_URL'), os.getenv('SUPABASE_KEY'))
-app.secret_key = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def home():
     user_logged_in = 'user_id' in session
-    return render_template('home.html', logged_in=user_logged_in)
+    user_type = session.get('user_type', None)
+    return render_template('home.html', logged_in=user_logged_in, user_type=user_type)
 
 @app.route('/sign_up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
         email = request.form['email']
         password = request.form['password']
-        user_data = {'email': email, 'password': password}
+        user_type = request.form['user_type']
+        user_data = {'email': email, 'password': password, 'options': {'data': {'user_type': user_type}}}
 
         try:
             response = app.supabase.auth.sign_up(user_data)
@@ -43,7 +47,9 @@ def login():
 
         try:
             response = app.supabase.auth.sign_in_with_password(user_data)
-            session['user_id'] = response.user.id  
+            session['user_id'] = response.user.id  # Armazenar o ID do usuário na sessão
+            user_metadata = response.user.user_metadata
+            session['user_type'] = user_metadata.get('user_type')
             return redirect(url_for('home'))
         except Exception as e:
             return str(e)
@@ -61,13 +67,28 @@ def welcome():
 def protected():
     if 'user_id' not in session:
         return redirect(url_for('login'))
-        
+    
+    user_type = session.get('user_type')
+    if user_type not in ['teacher', 'manager']:
+        return "Acesso negado."
+
     return "Esta é uma página protegida. Você está logado."
+
+@app.route('/manage_students')
+def manage_students():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+
+    user_type = session.get('user_type')
+    if user_type not in ['manager']:
+        return "Acesso negado."
+    
+    return "Página de gerenciamento de alunos."
 
 @app.route('/logout')
 def logout():
-    response = app.supabase.auth.sign_out()
     session.pop('user_id', None)
+    session.pop('user_type', None)
     return redirect(url_for('home'))
 
 if __name__ == "__main__":
